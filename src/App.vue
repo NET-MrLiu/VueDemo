3939d292464b9c476cfdced579a79eee7329f5c1
刘盈 <1263586498@qq.com>
2019-09-09 22:06:37 +0800

    主页基本完成

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,33 @@
 <template>
   <div id="app">
     <router-view />
+    <van-tabbar route>
+  <van-tabbar-item
+    replace
+    to="/home"
+    icon="home-o"
+  >
+    主页
+  </van-tabbar-item>
+  <van-tabbar-item
+    replace
+    to="/search"
+    icon="user-o"
+  >
+    我的信息
+  </van-tabbar-item>
+</van-tabbar>
   </div>
 </template>
 
 <style lang="scss">
 
 </style>
+<script>
+import Vue from 'vue';
+import { Tabbar, TabbarItem } from 'vant';
+Vue.use(Tabbar).use(TabbarItem);
+export default {
+  
+}
+</script>
--- src/views/index.vue
+++ src/views/index.vue
@@ -3,12 +3,25 @@
     <van-nav-bar @click-left="onClickLeft" title="主页" left-text="返回" left-arrow>
       <van-icon @click="onClickRight" name="search" slot="right" />
     </van-nav-bar>
-    <div id="top" >
+    <!-- <div id="top" >
       <span>工号:2849</span>
       <span>姓名:刘盈</span>
       <span>部门:财务数据中心</span>
-    </div>
-    <van-grid :column-num="4" square>
+    </div> -->
+    <!-- <van-swipe :autoplay="3000" indicator-color="white" style="height:190px;">
+  <van-swipe-item>1</van-swipe-item>
+  <van-swipe-item>2</van-swipe-item>
+  <van-swipe-item>3</van-swipe-item>
+  <van-swipe-item>4</van-swipe-item>
+</van-swipe> -->
+<van-swipe :autoplay="3000" style="height:190px;">
+  <van-swipe-item v-for="(image, index) in images" :key="index">
+    <img v-lazy="image" />
+  </van-swipe-item>
+</van-swipe>
+</van-swipe>
+</van-swipe>
+    <van-grid :column-num="3" square>
       <van-grid-item v-for="value in listdata" :key="value.id" v-bind:icon="value.icon" v-bind:text="value.name" v-bind:to="value.url" />
     </van-grid>
   </div>
@@ -33,15 +46,27 @@ import { NavBar } from "vant";
 Vue.use(NavBar);
 import { Toast } from "vant";
 Vue.use(Toast);
+import { Swipe, SwipeItem } from 'vant';
+Vue.use(Swipe).use(SwipeItem);
+import { Lazyload } from 'vant';
+Vue.use(Lazyload);
+import { Notify } from 'vant';
+Vue.use(Notify);
+
 
 export default {  
+
 data: function() {
     return {
-      items: [
-      { message: 'Foo' },
-      { message: 'Bar' }
-    ],
-      listdata:[{id:"1",name:"在线提单",url:"/",icon:"home-o"}]
+      listdata:[{id:"1",name:"在线提单",url:"/",icon:"home-o"},{id:"2",name:"撞单查询",url:"/",icon:"search"},{id:"3",name:"消息",url:"/",icon:"envelop-o"}
+      ,{id:"4",name:"新签",url:"/",icon:"balance-list-o"},{id:"5",name:"续签",url:"/",icon:"todo-list-o"},{id:"6",name:"首保",url:"/",icon:"bill-o"}
+      ,{id:"7",name:"服务包",url:"/",icon:"balance-o"},{id:"8",name:"网销宝",url:"/",icon:"refund-o"},{id:"9",name:"自有产品",url:"/",icon:"after-sale"}],
+      images: [
+        'https://myphotos1102.oss-cn-shenzhen.aliyuncs.com/banner1.png',
+        'https://myphotos1102.oss-cn-shenzhen.aliyuncs.com/banner2.png',
+         'https://myphotos1102.oss-cn-shenzhen.aliyuncs.com/banner3.png',
+        'https://myphotos1102.oss-cn-shenzhen.aliyuncs.com/banner4.png'
+      ]
     };
 },
 
